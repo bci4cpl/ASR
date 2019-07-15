7bab7e553e03174a60619aec2b394a9c539e46f5
Nir Getter (106) <getter@post.bgu.ac.il>
2019-07-15 10:49:47 +0300

    epoch data function

---
+++ epoch_data.m
@@ -0,0 +1,20 @@
+% epoch data
+function [epoched, N_win, Num_of_Windows] = epoch_data(data, t, Fs)
+% Epoching a contineous data
+%
+% In: 
+%       data:   EEG data to be epoched channels x samples
+%       t:      single epoch (window) time in miliseconds
+%       Fs:     frequency of samples (samples per second)
+%
+% Out:
+%       epoched:    EEG data epoched with channels x samples x epochs
+%       N_win:      Number of samples in each epoch (windows) equivalent to
+%                   size(epoched,2)                                             
+%       Num_of_Windows: number of epoche generated. equivalent to
+%       size(epoched,3)
+    N_win = round(t*Fs/1000); % number of samples in each 0.5-sec window
+    Num_of_Windows = floor(size(Yc,2)/N_win);
+    N_ch = size(Yc,1);
+    epoched = reshape(data(:,1:Num_of_Windows*N_win),N_ch,N_win,Num_of_Windows);
+end
